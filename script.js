b63e4373f43ac04fe5ab35ac8b6f45e6eb6e2605
theyogabook <107093216+theyogabook@users.noreply.github.com>
2022-10-11 21:15:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -221,3 +221,4 @@ window.addEventListener("load", function(){
 document.querySelector("#close").addEventListener("click", function(){
   document.querySelector(".popup").style.display = "none";
 });
+
